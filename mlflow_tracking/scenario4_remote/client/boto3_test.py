28d08ed690a491dc639c25c638bb013a03da7c3f
knakamura <c60evaporator@gmail.com>
2021-11-28 03:34:18 +0900

    Add boto3_test.py

---
+++ mlflow_tracking/scenario4_remote/client/boto3_test.py
@@ -0,0 +1,14 @@
+# %%
+import boto3
+import os
+# アップロード用のテキストファイル作成
+text = "rooms, zipcode, median_price, school_rating, transport"
+text_path = 'fuga.txt'
+with open(text_path, "w") as f:
+    f.write(text)
+# S3にアップロード
+s3 = boto3.resource('s3')
+bucket = s3.Bucket('mlflow20211128')
+bucket.upload_file(text_path, text_path)
+os.remove(text_path)
+# %%
