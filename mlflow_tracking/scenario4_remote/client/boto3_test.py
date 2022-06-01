ead2956a184e8d4464cf5dc484948d078a5cb9ed
knakamura <c60evaporator@gmail.com>
2022-06-02 01:28:37 +0900

    Modify boto3_test.py

--- mlflow_tracking/scenario4_remote/client/boto3_test.py
+++ mlflow_tracking/scenario4_remote/client/boto3_test.py
@@ -7,7 +7,7 @@ text_path = 'fuga.txt'
 with open(text_path, 'w') as f:
     f.write(text)
 # S3にアップロード
-s3 = boto3.resource('s3', aws_access_key_id='', aws_secret_access_key='')
+s3 = boto3.resource('s3')
 bucket = s3.Bucket('mlflow-policy-test')
 bucket.upload_file(text_path, text_path)
 os.remove(text_path)
